d4b242d0c4e9f2c41372dbc263fd7a192869e49b
SkorobagatiyMykola <nick1980457@gmail.com>
2024-02-04 19:18:45 +0200

    Improved code (2), added function printDate

--- src/main/java/com/skorobagatiy/service/Sinoptic.java
+++ src/main/java/com/skorobagatiy/service/Sinoptic.java
@@ -9,40 +9,20 @@ import java.io.IOException;
 
 public class Sinoptic {
     private static final String url = "https://ua.sinoptik.ua/";
-    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
 
     public static void main(String[] args) {
         //System.out.println(getPage());
 
         // System.out.println("  Ніч    Ранок    День   Вечір");
-        int COUNT =45;
-       // System.out.println("  Температура    Тиск    Вологість   Вітер");
+        int COUNT = 45;
+        // System.out.println("  Температура    Тиск    Вологість   Вітер");
         Document page = getPage();
-        //Element tabl = page.select("div.tabs").first();
-        Element tabl = page.select("#blockDays").first();
-
-        //Elements date = tabl.select(".date ");
-
-        Elements date = tabl.select(".main ");
+        Element tab = page.select("#blockDays").first();
+        Elements date = tab.select(".main ");
 
         printDate(date);
-
-
-        Elements months = tabl.select(".month");
-        Elements months2 = tabl.select("p[month]");
-        Elements temperature = tabl.select(".temperature");
-//        Elements temperature2 = tabl.select("div[temperature]");
-
-       // System.out.println(date.size());
-        int z=2;
-
-//        for (int i = 1; i <=7 ; i++) {
-//            String str = "bd"+i;
-//            tabl.
-//
-//        }
-
-        // css query language
 
         System.out.println("==================================");
     }
@@ -58,14 +38,30 @@ public class Sinoptic {
         return page;
     }
 
-    private static void printDate(Elements list){
+    private static void printDate(Elements list) {
+        String dateMainFormat = "%s %s (%s) %s, температура %s %s";
 
-        for (Element el: list) {
-            String day_week = el.select(".day-link").text();
-            System.out.println(day_week);
-            String date_number = el.select(".date ").text();
-            //System.out.println(date_number);
+        for (Element el : list) {
 
+            String dayNumber = el.select(".date ").text();
+            String dayMonth = el.select(".month ").text();
+            String dayWeek;
+
+            if (el.select("p.date.dateFree").hasText())
+                dayWeek = ANSI_RED + el.select(".day-link").text() + ANSI_RESET;
+            else
+                dayWeek = el.select(".day-link").text();
+
+            String weather = el.select(".weatherIco ").attr("title").toString();
+
+            Element temp = el.select(".temperature").first();
+            Element min = temp.select(".min").first();
+            Element max = temp.select(".max").first();
+            String minTemp = min.select("span").text();
+            String maxTemp = max.select("span").text();
+
+            String line = String.format(dateMainFormat, dayNumber, dayMonth, dayWeek, weather, minTemp, maxTemp);
+            System.out.println(line);
         }
 
     }
