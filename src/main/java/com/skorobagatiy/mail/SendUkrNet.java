4462ce611c9c533b24ea4511948f1e780a31967f
SkorobagatiyMykola <nick1980457@gmail.com>
2024-02-11 13:24:09 +0200

    Added URL param

--- src/main/java/com/skorobagatiy/mail/SendUkrNet.java
+++ src/main/java/com/skorobagatiy/mail/SendUkrNet.java
@@ -1,6 +1,7 @@
 package com.skorobagatiy.mail;
 
 public class SendUkrNet {
+    private static String URL = "";
     public static void main(String[] args) {
         System.out.println("Seng email from ukr.net");
     }
