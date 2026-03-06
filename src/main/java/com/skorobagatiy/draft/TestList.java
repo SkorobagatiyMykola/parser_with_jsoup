6fb17c7674bb6ffe0a8f938452059e2cf4f503e3
SkorobagatiyMykola <nick1980457@gmail.com>
2026-03-06 13:28:15 +0200

    Updated code

---
+++ src/main/java/com/skorobagatiy/draft/TestList.java
@@ -0,0 +1,26 @@
+package com.skorobagatiy.draft;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestList {
+
+    private final static String[] arrayFactorialStr = new String[30];
+
+    static {
+        arrayFactorialStr[1] = "one";
+        arrayFactorialStr[3] = "three";
+        arrayFactorialStr[5] = "five";
+    }
+
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 22; i++) {
+            System.out.println("arrayFactorialStr[" + i + "]=\"1\";" );
+        }
+
+        //Arrays.stream(arrayFactorialStr).forEach(System.out::println);
+    }
+}
